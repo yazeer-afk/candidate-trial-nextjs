d95debc9803848828b3bb4f7cb261e99e9e32e6e
yazeerr <int.yazeer@gmail.com>
2024-02-17 03:06:58 +0530

    feature: Implemented the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import Button from "@/components/Button";
 import styles from "./page.module.scss";
 
 export default function Home() {
-    return <main className={styles.main}></main>;
+    return (
+        <main>
+            <section className={styles['hero-container']}>
+                <h1>The ultimate atomix starter theme to help <br /> your business grow faster!</h1>
+                <Button label="Contact us" />
+            </section>
+            <section>Column with image</section>
+            <section>Carousal section</section>
+            <section>Team section</section>
+        </main>
+    );
 }
