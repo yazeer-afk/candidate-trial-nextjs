a64d5a515550ed52ba07e082c3f44acfb6ef6fb7
yazeerr <int.yazeer@gmail.com>
2024-02-18 20:53:40 +0530

    feature: defined the data files

---
+++ src/data/getData.ts
@@ -0,0 +1,56 @@
+import fsPromises from "fs/promises";
+import path from "path";
+
+// Footer structure ------------------------
+interface IFooterStructure {
+    label: string;
+    content: string[];
+}
+
+// Team structure --------------------------
+interface ITeamStructure {
+    name: string;
+    title: string;
+    img: string;
+}
+
+// Carousal structure --------------------------
+interface ICarousalStructure {
+    title: string;
+    description: string;
+    img: string;
+}
+
+// Nav structure --------------------------
+interface INavStructure {
+    name: string;
+    img: string;
+    content: {
+        title: string;
+        items: string[]
+    }[];
+}
+
+const basePath = "/src/data";
+
+const readJsonFile = async <T>(name: string): Promise<T[]> => {
+    const filePath = path.join(process.cwd(), `${basePath}${name}`)
+    const jsonData = await fsPromises.readFile(filePath);
+    return JSON.parse(jsonData.toString()) as T[];
+};
+
+export const getTeamContent = async (): Promise<ITeamStructure[]> => {
+    return readJsonFile<ITeamStructure>('/team-content.json');
+};
+
+export const getFooterContent = async (): Promise<IFooterStructure[]> => {
+    return readJsonFile<IFooterStructure>("/footer-content.json");
+};
+
+export const getCarousalContent = async (): Promise<ICarousalStructure[]> => {
+    return readJsonFile<ICarousalStructure>("/carousal-content.json");
+};
+
+export const getNavContent = async (): Promise<INavStructure[]> => {
+    return readJsonFile<INavStructure>('/nav-content.json');
+};
